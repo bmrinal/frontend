989ee1d7064db02c90401297672e61913d30beae
Arun Selvaraj <selvaraj.arun@gmail.com>
2013-05-13 23:58:28 -0700

    authentication basics

--- WorkProtocolFrontend/war/js/vendorprofile.js
+++ WorkProtocolFrontend/war/js/vendorprofile.js
@@ -1,4 +1,29 @@
 $(function (){
+
+	$.ajax({
+		  url: "http://work0protocol.appspot.com/resources/user",
+		  dataType: 'json',
+		  xhrFields: {
+			  withCredentials: true
+		  },
+		  cache: false,
+		  complete: function (){
+			  $('.page-loading').hide();
+		  },
+		  success: function (response){
+			  if(response && response.userId){
+				  console.log(response.nickname);
+			  } else {
+				  window.location.href = response.signInUrl + '?ru=' + window.location.href;
+			  }
+		  },
+		  error: function (e){
+			 $('#page-status').html('Sorry, unable to authenticate')
+							.addClass('alert-error')
+							.show();
+		  }
+	});
+
 	$('#profileForm').submit(function (e){
 		e.preventDefault();
 		$.ajax({
